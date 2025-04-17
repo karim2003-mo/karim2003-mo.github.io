9aff94099fa583f0c2710299594ea305b54e4cb8
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2025-04-17 18:30:13 +0200

    v9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3808202425"
+    serviceWorkerVersion: "3681902475"
   }
 });
